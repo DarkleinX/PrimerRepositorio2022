7b6c854455c666d1e3d67b45781469e795a3308f
DarkleinX <josue.sifuentes.2002@gmail.com>
2022-03-19 17:13:09 -0500

    primeraPruebaGit

---
+++ src/main/java/com/usmp/fia/usmp/AlgoritmoTrabajoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.usmp.fia.usmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoritmoTrabajoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoritmoTrabajoFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/usmp/fia/usmp/AlgoritmoTrabajoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usmp.fia.usmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoritmoTrabajoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
