ff8d597f53fe10791aeda6e158110e260c1f27ab
DarkleinX <josue.sifuentes.2002@gmail.com>
2022-03-19 17:41:16 -0500

    Añado Index/ambola fecha

--- src/main/java/com/usmp/fia/usmp/controller/Index.java
+++ src/main/java/com/usmp/fia/usmp/controller/Index.java
@@ -1,6 +1,15 @@
 package com.usmp.fia.usmp.controller;
 
+import org.springframework.ui.Model;
+
 public class Index {
 	//bla bla blaasdasdad
-	//Texto escrito a las 17:36
+
+	//este index lo cree a las 17:40
+	public String index(Model model) {
+		
+		return "frmIndex";
+		
+	}
+	
 }
